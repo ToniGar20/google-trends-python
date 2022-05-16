b6289aa227a3e92f3503c57b96cb73df91e2d9df
ToniGar20 <tgarcia@habitissimo.com>
2022-05-16 12:27:49 +0200

    added code to export data to GSheet

--- category-trends/category-trends.py
+++ category-trends/category-trends.py
@@ -1,5 +1,10 @@
 from pytrends.request import TrendReq
+from oauth2client.service_account import ServiceAccountCredentials
+from df2gspread import df2gspread as d2g
+import pandas as pd
 import csv
+import gspread
+
 
 # Documentation of Google Trends categories
 # https://github.com/pat310/google-trends-api/wiki/Google-Trends-Categories
@@ -47,16 +52,19 @@ def get_trends(trend_category, country):
         geo=country
     )
     data = pytrend.interest_over_time()
-    data['category'] = TRENDS_CATEGORIES[category]
+    data['category'] = TRENDS_CATEGORIES[trend_category]
     data['country'] = country
     del data['isPartial']
     return data
 
+
+# Writing data into CSV
+#
 # Headers for file
 file_header = ['date', 'trend_value', 'category', 'country']
 
 with open('category-trends-results.csv', 'w') as file:
-    file.truncate() # Removing content of current CSV if there is old one
+    file.truncate()  # Removing content of current CSV if there is old one
     writer = csv.writer(file)
     writer.writerow(file_header)
 
@@ -66,3 +74,18 @@ with open('category-trends-results.csv', 'w') as file:
             category_data = get_trends(category, item)
             print(category_data)
             category_data.to_csv('category-trends-results.csv', header=None, mode='a')
+
+# Sending CSV data to Google SpreadSheet
+#
+# Data for autorization with user fo Google API
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('py-for-gsheets-ed724968a63d.json', scope)
+gc = gspread.authorize(credentials)
+
+# Drive key and worksheet
+spreadsheet_key = '10RBqKVfXuNViou5LUBjE3-tHfqjkZckEIncnHBlJOMk'
+wks_name = 'Data'
+
+# Reading CSV and writing df2gspread package
+df = pd.read_csv('category-trends-results.csv')
+d2g.upload(df, spreadsheet_key, wks_name, credentials=credentials, row_names=True)
